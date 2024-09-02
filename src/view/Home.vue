654e31f2923238ab819393f2ce4c164de5f33fb9
STEFANO LAZZARI <stefanolazzari79@libero.it>
2024-09-02 17:54:30 +0200

    add content

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,20 +1,34 @@
 <script setup>
-import BodyComponent from "../components/BodyComponent.vue";
+import BodyComponent from '../components/BodyComponent.vue'
 </script>
 
 <template>
   <BodyComponent>
     <!-- <img src="/src/assets/20220129_221200_2.jpg" alt="" width="200" style="float:left; margin:10px"> -->
-    <p> Sono un insegnate di informatica e coding. Mi occupo di didattica da 15 anni in contesti multidisciplinare in
-      cui le nuove tecnologie ed il coding fanno da collante tra le varie attività. <br>
-
-      Le lezioni si basano su attività pratiche come la costruzione di un sito analizzando le basi dell’html, css e
-      javascript. L’utilizzo poi e di librerie e framework più specifici come vue, node, laravel ecc. da la possibiltà
-      di avere un accesso a quello che è il panorama attuale dello sviluppo software. 
+  <div>
+<br>
+    <p>
+      Le lezioni si basano su attività pratiche come la costruzione di un sito analizzando le basi
+      dell’html, css e javascript. L’utilizzo poi e di librerie e framework più specifici come vue,
+      node, laravel ecc. da la possibiltà di avere un accesso a quello che è il panorama attuale
+      dello sviluppo software.
     </p>
-
-
-
-
+<br>
+    <p>
+      Nell’ambito del mio ruolo di docente, attribuisco grande importanza all’insegnamento
+      inclusivo, ponendo particolare attenzione agli studenti che si trovano in situazioni familiari
+      difficili o che presentano certificazioni di ADHD e Autismo. 
+    </p>
+    <br>
+    <p class="pb-20">
+      Mi impegno a creare un ambiente
+      di apprendimento che risponda alle esigenze di ogni studente, utilizzando metodologie
+      didattiche differenziate e strumenti digitali personalizzati per supportare il loro percorso
+      di crescita e apprendimento. L’obiettivo è quello di garantire a tutti gli studenti,
+      indipendentemente dalle loro difficoltà, l’accesso a un’istruzione di qualità e la possibilità
+      di esprimere il loro potenziale.
+    </p>
+    
+  </div>
   </BodyComponent>
 </template>
