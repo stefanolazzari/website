e4b3fedcf97f2d84e4dfcb3bf28a72a8817d4dc3
STEFANO LAZZARI <stefanolazzari79@libero.it>
2024-09-02 17:51:24 +0200

    response on mobile and lg, add dinamic h1

--- src/components/BodyComponent.vue
+++ src/components/BodyComponent.vue
@@ -1,25 +1,16 @@
 <template>
-    <div>
-      
-      <div class="sm:pl-2 sm:pr-2 sm:pt-20 lg:pl-60 lg:pr-60 lg:pt-20">
-        
-      <!-- <h1 class="p-5">{{this.$route.name}}</h1> -->
-        <!-- <div class="grid gap-2 sm:grid-cols-1 lg:grid-cols-1"> -->
-         <div class="text-justify items-center rounded-md">
-            
-             <p><slot /></p>
-            
-         </div> 
-         
+  <div>
+    <div class="sm:pl-2 sm:pr-2 sm:pt-20  md:pl-40 md:pr-40 md:pt-10  lg:pl-60 lg:pr-60 lg:pt-20">
+      <div class="bg-white/50 rounded p-7 ">
+        <h1 class="pb-7">{{ this.$route.name }}</h1>
+        <div class="grid gap-2 sm:grid-cols-1 lg:grid-cols-1">
+          <div class="text-justify items-center ">
+            <p>
+              <slot />
+            </p>
+          </div>
+        </div>
       </div>
-     </div>
-    <!-- </div> -->
-  </template>
-  
-  <script>
-  
-  </script>
-
-  <style>
-
-</style>
+    </div>
+  </div>
+</template>
